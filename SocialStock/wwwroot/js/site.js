e78287c3c5c49d322e1547c4fdea345047b1d0fc
chetannaveen <chetannaveenv@gmail.com>
2022-11-20 21:06:57 -0500

    Added javascript to remove colors with a button

--- SocialStock/wwwroot/js/site.js
+++ SocialStock/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function changeme() {
+    document.getElementById("refresh").disabled = false;
+    document.getElementById("colors").disabled = true;
+    var nodes = document.getElementById("colorDiv").getElementsByTagName('li');
+    for (var i = 0; i < nodes.length; i++) {
+        nodes[i].style.background = '#f3f3f3';
+        nodes[i].style.color = '#000000';
+
+    }
+}
